32c4fe50e3586a2f1277c1fb61838ef4763f3bde
Christian Garbs <mitch@cgarbs.de>
2017-08-14 22:14:35 +0200

    add simple diskfree check
    not configurable yet

---
+++ check/wrapper/diskfree.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Filesys::Df;
+use Sys::Filesystem;
+
+use Data::Dumper;
+
+foreach my $mountpoint (Sys::Filesystem->new->mounted_filesystems) {
+
+    my $df = df($mountpoint, 1024);
+    next unless defined $df;
+
+    my ($warn, $crit, $status) = (0, 0);
+    
+    my $block_p = $df->{bfree} / $df->{blocks} * 100;
+    
+    if ($block_p < 2) {
+	$crit++;
+    }
+    elsif ($block_p < 5) {
+	$warn++;
+    }
+
+    $status = sprintf '%3.0d%% free blocks', $block_p;
+
+    if (exists $df->{files}) {
+	my $inode_p = $df->{ffree} / $df->{files} * 100;
+
+	if ($inode_p < 2) {
+	    $crit++;
+	}
+	elsif ($inode_p < 5) {
+	    $warn++;
+	}
+
+	$status .= sprintf ', %3.0d%% free inodes', $inode_p;
+    }
+
+    my $sev;
+    if ($crit) {
+	$sev = 'C';
+    }
+    elsif ($warn) {
+	$sev = 'W';
+    }
+    else {
+	$sev = 'I';
+    }
+    
+    printf "%s:diskfree:%s on %s\n", $sev, $status, $mountpoint;
+}
